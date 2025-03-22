dedda30d5e28ce758cf91cbb7e019317d6c40881
Ram Babu <ram.kottapally@gmail.com>
2025-03-22 21:30:02 +0530

    added quote to footer and improved dark mode functionality

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 function myFunction() {
     var element = document.body;
     var pageHeader = document.querySelector('.page-header');
+    var pageFooter = document.querySelector('.page-footer');
     var sectionLabel = document.getElementsByClassName('section-label');
     var headerLabel = document.getElementsByClassName('header-label');
     element.classList.toggle("dark-mode");
     pageHeader.classList.toggle("dark-mode");
+    pageFooter.classList.toggle("dark-mode");
     sectionLabel.classList.toggle("dark-mode-labels");
     headerLabel.classList.toggle("dark-mode-labels");
  }
