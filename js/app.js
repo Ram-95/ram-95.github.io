c1ee86dd1c24eef7ec2400490f72ea4322955199
Ram Babu <ram.kottapally@gmail.com>
2025-03-22 22:37:18 +0530

    enhanced dark mode functionality and updated layout with new tech stack details

--- js/app.js
+++ js/app.js
@@ -2,11 +2,18 @@ function myFunction() {
     var element = document.body;
     var pageHeader = document.querySelector('.page-header');
     var pageFooter = document.querySelector('.page-footer');
-    var sectionLabel = document.getElementsByClassName('section-label');
-    var headerLabel = document.getElementsByClassName('header-label');
+    var sectionLabels = document.getElementsByClassName('section-label');
+    var headerLabels = document.getElementsByClassName('header-label');
+    var toggleButton = document.querySelector('.toggle-button');
     element.classList.toggle("dark-mode");
     pageHeader.classList.toggle("dark-mode");
     pageFooter.classList.toggle("dark-mode");
-    sectionLabel.classList.toggle("dark-mode-labels");
-    headerLabel.classList.toggle("dark-mode-labels");
- }
+
+    for (var i = 0; i < sectionLabels.length; i++) {
+        sectionLabels[i].classList.toggle("dark-mode-labels");
+    }
+
+    for (var i = 0; i < headerLabels.length; i++) {
+        headerLabels[i].classList.toggle("dark-mode-labels");
+    }
+}
